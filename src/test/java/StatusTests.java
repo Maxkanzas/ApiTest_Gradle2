b44bc2f9c73614eace93f03712956387000b5b87
MaxxPC <maxkanz1991@gmail.com>
2025-01-07 18:00:18 +0400

    change StatusTest

---
+++ src/test/java/StatusTests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.core.Is.is;
+
+public class StatusTests {
+    @Test
+    public void checkStatus() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", equalTo(20))
+                .body("browsers.chrome", hasKey("113.0"));
+    }
+}
