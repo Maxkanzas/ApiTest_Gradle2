b6163c1de858cb87be9e882b36117b933292a130
maxkanzas <maxkanz1991@gmail.com>
2025-01-21 22:43:13 +0400

    change CollectionTests

--- src/test/java/tests/demoqabookstore/CollectionTests.java
+++ src/test/java/tests/demoqabookstore/CollectionTests.java
@@ -1,50 +1,48 @@
 package tests.demoqabookstore;
-import api.AuthorizationApiStep;
+
 import api.BookApiSteps;
 import models.books.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.ProfilePage;
+
 import static io.qameta.allure.Allure.step;
 
 public class CollectionTests extends TestBase {
     ProfilePage profilePage = new ProfilePage();
-    AuthorizationApiStep authorizationApiStep = new AuthorizationApiStep();
     BookApiSteps bookApiSteps = new BookApiSteps();
-    Credentials credentials = new Credentials();
     AddBookList addBookList = new AddBookList();
     DataUsers dataUsers = new DataUsers();
-    DataBook dataBook = new DataBook();
-    Isbn isbn = new Isbn();
 
     @Test
     @DisplayName("Авторизация, удаление всех книг из корзины и добавление новых, UI проверка книг в профиле")
     public void addBookToCollectionDeletedAllBooksTest() {
-        step("Удаление всех книг из корзины", ()-> {
+        step("Удаление всех книг из корзины", () -> {
             bookApiSteps.deleteAllBooks(dataUsers);
         });
-        step("Добавление новых книг в корзину", ()-> {
+        step("Добавление новых книг в корзину", () -> {
             bookApiSteps.addBook(dataUsers, addBookList);
         });
-        step("Проверяем наличие добавленной книги в корзине", ()-> {
+        step("Проверяем наличие добавленной книги в корзине", () -> {
             profilePage.openPageProfile();
             profilePage.verifyTableContainsText("Speaking JavaScript");
         });
     }
+
     @Test
     @DisplayName("Авторизация, удаление всех книг, добавление новых, UI проверка книг в профиле, удаление одной любой книги")
     public void addBookToCollectionwithDeleteOneBookTest() {
-        step("Удаление всех книг из корзины", ()-> {
+        step("Удаление всех книг из корзины", () -> {
             bookApiSteps.deleteAllBooks(dataUsers);
         });
-        step("Добавление новых книг в корзину", ()-> {
+        step("Добавление новых книг в корзину", () -> {
             bookApiSteps.addBook(dataUsers, addBookList);
         });
-        step("Проверяем наличие добавленной книги в корзине", ()-> {
+        step("Проверяем наличие добавленной книги в корзине", () -> {
             profilePage.openPageProfile();
             profilePage.verifyTableContainsText("Programming JavaScript Applications");
         });
-        step("Удаление одной книги из корзины", ()-> {
+        step("Удаление одной книги из корзины", () -> {
             bookApiSteps.deleteBook(dataUsers, "Programming JavaScript Applications");
         });
 
